1872bf856eeca4584e6a2e4941c78850fab49dd5
PsykeDady <psdady@msn.com>
2021-08-07 18:19:05 +0200

    fix file manager

--- constants.sh
+++ constants.sh
@@ -8,7 +8,7 @@ export kservices_local_path="$HOME/.local/share/kservices5/"
 export kservices_mowish_local_path="$kservices_local_path/mowish.desktop"
 export kservices_resource_path="resources/mowish_dolphine.desktop"
 export nautilus_scripts_path="$HOME/.local/share/nautilus/scripts"
-export nautilus_mowish_resource="resource/mowish_nautilus.sh"
+export nautilus_mowish_resource="resources/mowish_nautilus.sh"
 
 ### TRANSLATIONS 
 export TRANSLATION_DIR="translations"
