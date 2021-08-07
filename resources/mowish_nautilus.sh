56a06ea0e046574e598e3c8fc8c12fcb92ed3977
PsykeDady <psdady@msn.com>
2021-08-07 19:45:54 +0200

    fix noactoin mowish nautilus

--- resources/mowish_nautilus.sh
+++ resources/mowish_nautilus.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-if (( $# <1 )); then 
-    mowish 
-fi
+noaction=1;
 
 while (( $# > 0 )); do
     
     file=$1
     if [[ -d "$file" ]]; then 
         mowish "$1";
+        noaction=0
     fi;
     shift 
 done
+
+if (( noaction==1 )); then 
+    mowish
+fi
 
