55ca9a73d57bdd758a45e1064ce72799314edfec
PsykeDady <psdady@msn.com>
2021-08-08 18:20:13 +0200

    fix nemo action local path

--- constants.sh
+++ constants.sh
@@ -9,7 +9,7 @@ export kservices_mowish_local_path="$kservices_local_path/mowish.desktop"
 export kservices_resource_path="resources/mowish_dolphine.desktop"
 export nautilus_scripts_path="$HOME/.local/share/nautilus/scripts"
 export nautilus_mowish_resource="resources/mowish_nautilus.sh"
-export nemo_action_local_path="$HOME/.local/share/nemo/"
+export nemo_action_local_path="$HOME/.local/share/nemo/actions"
 export nemo_action_mowish_local_path="$nemo_action_local_path/mowish.nemo_action"
 export nemo_action_resource_path="resources/mowish_nemo.nemo_action"
 
