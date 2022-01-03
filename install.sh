c7f21cb607132922ab5e3a13651ed09a59074911
PsykeDady <psdady@msn.com>
2022-01-03 12:10:32 +0100

    delete Thunar Action moved in utils file

--- install.sh
+++ install.sh
@@ -235,36 +235,7 @@ function thunarAction(){
 			return 0 
 		fi
 
-		nlf=$(awk  '
-		BEGIN{
-				mowish_section=0;
-		}
-		{
-				if ( $0 ~ "mowish" ) {
-						row_mowish=NR;
-						mowish_section=1;
-				}
-				if( mowish_section==0 ) {
-						if($0=="<action>")
-								last_action=NR; 
-				} else if(mowish_section==1) {
-						if($0=="</action>") {
-								mowish_section=2;
-								last_action_end=NR;
-						}
-				}
-		} 
-		END {
-				print((last_action-1)";"row_mowish";"last_action_end";"NR-last_action_end);
-		}' "${thunar_mowish_local_path:?}")
-
-		headuca=$(cut -d ';' -f1 <<< "$nlf");
-		tailuca=$(cut -d ';' -f4 <<< "$nlf");
-
-		headuca=$(head -"$headuca" "${thunar_mowish_local_path:?}");
-		tailuca=$(tail -"$tailuca" "${thunar_mowish_local_path:?}");
-
-		infomsg "$headuca\n$tailuca" | tee "${thunar_mowish_local_path:?}"
+		thunar_mowish_local_path;
 
 	fi
 	nl=$(grep -c "" "${thunar_mowish_local_path:?}"| cut -d' ' -f1)
--- utils.sh
+++ utils.sh
@@ -13,3 +13,38 @@ function infomsg() {
 function error (){
 	echo -e "[ERR] : $*" > /dev/stderr
 }
+
+## delete thunar action, needed for install and uninstall
+function deleteThunarAction(){
+
+	nlf=$(awk  '
+		BEGIN{
+				mowish_section=0;
+		}
+		{
+				if ( $0 ~ "mowish" ) {
+						row_mowish=NR;
+						mowish_section=1;
+				}
+				if( mowish_section==0 ) {
+						if($0=="<action>")
+								last_action=NR; 
+				} else if(mowish_section==1) {
+						if($0=="</action>") {
+								mowish_section=2;
+								last_action_end=NR;
+						}
+				}
+		} 
+		END {
+				print((last_action-1)";"row_mowish";"last_action_end";"NR-last_action_end);
+		}' "${thunar_mowish_local_path:?}")
+
+		headuca=$(cut -d ';' -f1 <<< "$nlf");
+		tailuca=$(cut -d ';' -f4 <<< "$nlf");
+
+		headuca=$(head -"$headuca" "${thunar_mowish_local_path:?}");
+		tailuca=$(tail -"$tailuca" "${thunar_mowish_local_path:?}");
+
+		infomsg "$headuca\n$tailuca" | tee "${thunar_mowish_local_path:?}"
+}
